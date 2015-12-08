30fd0b2f9585a16bb8dde858cc460e0b8dedf46c
Kosta Harlan <kosta@kostaharlan.net>
2015-12-08 10:40:35 -0500

    Coding standards

--- sumac.php
+++ sumac.php
@@ -29,13 +29,14 @@ $console
         $to = $from = $range;
     }
     $range = new Range($from, $to);
-    $output->writeln('<question>Syncing data for time period between ' . $from . ' and ' . $to . '</question>');
+    $output->writeln('<question>Syncing data for time period between '.$from.' and '.$to.'</question>');
 
     // Load the configuration.
     $yaml = new Yaml();
     $config = $yaml->parse(file_get_contents('config.yml'));
     if (!$config) {
         $output->writeln('<error>Could not load the config.yaml file.</error>');
+
         return;
     }
 
@@ -46,20 +47,24 @@ $console
     $harvest->setAccount($config['auth']['harvest']['account']);
 
     // Initialize the Redmine client.
-    $redmine_client = new Redmine\Client($config['auth']['redmine']['url'], $config['auth']['redmine']['user'], $config['auth']['redmine']['pass']);
+    $redmine_client = new Redmine\Client(
+        $config['auth']['redmine']['url'],
+        $config['auth']['redmine']['user'],
+        $config['auth']['redmine']['pass']
+    );
 
     // Get all project entries.
     $projects = $harvest->getProjects(Carbon::parse($from)->toDateTimeString());
-    $output->writeln('<info>Getting data for ' . count($projects->get('data')) . ' projects</info>');
+    $output->writeln('<info>Getting data for '.count($projects->get('data')).' projects</info>');
     $entries = [];
 
     // Get entries.
     foreach ($projects->get('data') as $project) {
         if (in_array($project->get('id'), $config['sync']['projects']['exclude'])) {
-            $output->writeln('<comment>- Skipping project ' . $project->get('name') . ', in exclude list</comment>');
+            $output->writeln('<comment>- Skipping project '.$project->get('name').', in exclude list</comment>');
             continue;
         }
-        $output->writeln('<comment>- Retrieving time entry data for ' . $project->get('name') . '</comment>');
+        $output->writeln('<comment>- Retrieving time entry data for '.$project->get('name').'</comment>');
         $project_entries = $harvest->getProjectEntries($project->get('id'), $range);
         foreach ($project_entries->get('data') as $entry) {
             $entries[] = $entry;
@@ -81,13 +86,19 @@ $console
         }
     }
 
-    $output->writeln(sprintf('<info>Found %d entries with possible Redmine IDs and %d without</info>', count($entries_to_log), count($entries_without_id)));
+    $output->writeln(
+        sprintf(
+            '<info>Found %d entries with possible Redmine IDs and %d without</info>',
+            count($entries_to_log),
+            count($entries_without_id)
+        )
+    );
 
     // Get all time entries from Redmine.
     $time_api = new Redmine\Api\TimeEntry($redmine_client);
 
     foreach ($entries_to_log as $entry) {
-        $output->writeln(sprintf("<info>Processing entry %d - %s</info>", $entry->get('id'), $entry->get('notes')));
+        $output->writeln(sprintf('<info>Processing entry %d - %s</info>', $entry->get('id'), $entry->get('notes')));
         // Load the Redmine issue and check if the Harvest time entry ID is there, if so, skip.
         $redmine_issue_numbers = [];
         preg_match('/#([0-9]+)/', $entry->get('notes'), $redmine_issue_numbers);
@@ -99,12 +110,12 @@ $console
             'limit' => 10000,
         ));
         if (isset($redmine_time_entries['total_count']) && $redmine_time_entries['total_count'] > 0) {
-          // There might be a match.
+            // There might be a match.
             foreach ($redmine_time_entries['time_entries'] as $rm_time_entry) {
                 if (strpos($rm_time_entry['comments'], $entry->get('id')) !== false) {
                     // There's a match, skip this entry.
                     // TODO: If `--update` is passed, update the time entry.
-                    $output->writeln('<comment>- There is already a time entry for ' . $entry->get('notes') . '</comment>');
+                    $output->writeln('<comment>- There is already a time entry for '.$entry->get('notes').'</comment>');
                     continue 2;
                 }
             }
@@ -113,22 +124,34 @@ $console
         $issue_api = new Redmine\Api\Issue($redmine_client);
         $redmine_issue = $issue_api->show($redmine_issue_number);
         if (!$redmine_issue || !isset($redmine_issue['issue']['project']['id'])) {
-          // Issue doesn't exist in Redmine; this is probably a GitHub issue reference.
+            // Issue doesn't exist in Redmine; this is probably a GitHub issue reference.
             $output->writeln(sprintf('<error>- Could not find Redmine issue %d!</error>', $redmine_issue_number));
             continue;
         }
 
         // Validate that issue ID exists in project.
-        if (isset($config['sync']['projects']['map'][$entry->get('project-id')]) && $config['sync']['projects']['map'][$entry->get('project-id')] !== $redmine_issue['issue']['project']['name']) {
-          // The issue number doesn't belong to the Harvest project we are looking at
+        if (isset($config['sync']['projects']['map'][$entry->get('project-id')])
+            && $config['sync']['projects']['map'][$entry->get('project-id')]
+            !== $redmine_issue['issue']['project']['name']) {
+            // The issue number doesn't belong to the Harvest project we are looking at
           // time entries for, so continue. It's probably a GitHub issue ref.
-            $output->writeln(sprintf("<comment>- Skipping entry for %d as it is out of range!</comment>", $entry->get('id')));
+            $output->writeln(
+                sprintf(
+                    '<comment>- Skipping entry for %d as it is out of range!</comment>',
+                    $entry->get('id')
+                )
+            );
             continue;
         }
 
         if (!isset($config['sync']['users'][$entry->get('user-id')])) {
             // No mapping is defined in the config, so throw an error and skip this entry.
-            $output->writeln(sprintf('<error>No mapping is defined for user %d, please adjust config.yaml</error>', $entry->get('user-id')));
+            $output->writeln(
+                sprintf(
+                    '<error>No mapping is defined for user %d, please adjust config.yaml</error>',
+                    $entry->get('user-id')
+                )
+            );
             continue;
         }
 
@@ -141,25 +164,39 @@ $console
             'activity_id' => 9,
             'project_id' => $redmine_issue['issue']['project']['id'],
             'hours' => $entry->get('hours'),
-            'comments' => $entry->get('notes') . ' [Harvest ID #' . $entry->get('id') . ']',
+            'comments' => $entry->get('notes').' [Harvest ID #'.$entry->get('id').']',
         );
 
         try {
-            $redmine_user = new Redmine\Client($config['auth']['redmine']['url'], $config['auth']['redmine']['user'], $config['auth']['redmine']['pass']);
+            $redmine_user = new Redmine\Client(
+                $config['auth']['redmine']['url'],
+                $config['auth']['redmine']['user'],
+                $config['auth']['redmine']['pass']
+            );
             $redmine_user->setImpersonateUser($config['sync']['users'][$entry->get('user-id')]);
             if (!$input->getOption('dry-run')) {
-
                 $time_api = new Redmine\Api\TimeEntry($redmine_user);
                 $time_api->create($params);
             }
-            $output->writeln(sprintf('<comment>Created new time entry for issue #%d with hours %s</comment>', $redmine_issue_number, $entry->get('hours')));
+            $output->writeln(
+                sprintf(
+                    '<comment>Created new time entry for issue #%d with hours %s</comment>',
+                    $redmine_issue_number,
+                    $entry->get('hours')
+                )
+            );
         } catch (Exception $e) {
-            $output->writeln(sprintf('<comment>Failed to create time entry for issue #%d!</comment>', $redmine_issue_number, $e->getMessage()));
+            $output->writeln(
+                sprintf(
+                    '<comment>Failed to create time entry for issue #%d!</comment>',
+                    $redmine_issue_number,
+                    $e->getMessage()
+                )
+            );
         }
     }
 
     $output->writeln('<question>All done!</question>');
-  })
-;
+  });
 
 $console->run();
