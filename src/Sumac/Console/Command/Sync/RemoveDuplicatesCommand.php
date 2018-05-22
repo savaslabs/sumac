8e6075d0cdce6756e14e45b516011e76a171f75e
Kosta Harlan <kosta@fastmail.com>
2018-05-22 13:27:42 -0400

    Issue #6291: Remove unused use

--- src/Sumac/Console/Command/Sync/RemoveDuplicatesCommand.php
+++ src/Sumac/Console/Command/Sync/RemoveDuplicatesCommand.php
@@ -2,13 +2,10 @@
 
 namespace Sumac\Console\Command\Sync;
 
-use Sumac\Config\Config;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
-use Redmine;
 
 class RemoveDuplicatesCommand extends BaseSyncCommand
 {
